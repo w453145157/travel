df466fb559a331d5e030a1dae8a4b048feb2d3f1
lichong <453145157@qq.com>
2019-05-26 15:35:15 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+import 'styles/common.css'
 import fastClick from 'fastclick'
 
 fastClick.attach(document.body)
