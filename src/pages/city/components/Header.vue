699efbfa7f19c4cb1d00ed1d36288dd41057461a
lichong <453145157@qq.com>
2019-05-27 23:05:40 +0800

    updaste

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="header">
-    <div class="title">
-      <router-link to="/">
-        <span class="iconfont back-home">&#xe624;</span>
-      </router-link>
-      <p>城市选择</p>
-    </div>
-    <div class="input-area">
-      <input type="text" placeholder="输入城市名或拼音" id="">
-    </div>
+    <router-link to="/">
+      <span class="iconfont back-home">&#xe624;</span>
+    </router-link>
+    <p>城市选择</p>
   </div>
 </template>
 <script>
@@ -20,31 +15,24 @@ export default {
   @import '~styles/varibles.styl'
   .header
     background: $bgColor
-    .title
-      position: relative
-      height: .68rem
-      width: 100%
+    position: relative
+    height: .68rem
+    width: 100%
+    color: #fff
+    text-align: center
+    .back-home
+      position: absolute 
+      width: .6rem 
+      height: 100%
+      left: 0
+      line-height: .68rem
+      top: 0
+      font-size: .3rem
       color: #fff
-      text-align: center
-      .back-home
-        position: absolute 
-        width: .6rem 
-        height: 100%
-        left: 0
-        line-height: .68rem
-        top: 0
-        font-size: .3rem
-        color: #fff
-      p
-        width: 60%
-        margin: 0 auto
-        font-size: .3rem
-        height: 100%
-        line-height: 0.68rem
-    .input-area
-      height: .68rem
-      input 
-        display: block
-        margin: .1rem .2rem
-        height: 100%
+    p
+      width: 60%
+      margin: 0 auto
+      font-size: .3rem
+      height: 100%
+      line-height: 0.68rem
 </style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" id="">
+  </div>
+</template>
+<script>
+export default {
+  name: "CitySearch"
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .68rem
+    padding: 0 .1rem
+    background: $bgColor
+    input 
+      height: .5rem
+      line-height: .5rem
+      width: 100%
+      text-align: center
+      border-radius: .08rem
+      padding: 0 .2rem
+      box-sizing: border-box
+</style>
