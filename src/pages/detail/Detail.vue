e297953a0f529499382a99cefc9cd38bf2d5371b
lichong <453145157@qq.com>
2019-06-03 22:38:32 +0800

    update

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="detail">
-    <detail-banner></detail-banner>
+    <detail-banner :name="sightName" :bannerImg="bannerImg" :gallaryImgs="gallaryImgs"></detail-banner>
     <detail-header></detail-header>
-    <detail-list></detail-list>
     <div class="content">
-      <detail-list :list="list"></detail-list>
+      <detail-list :list="categoryList"></detail-list>
     </div>
   </div>
 </template>
@@ -12,6 +11,7 @@
 import DetailBanner from "./components/Banner";
 import DetailHeader from "./components/Header";
 import DetailList from "./components/List";
+import axios from "axios";
 export default {
   name: "Detail",
   components: {
@@ -21,34 +21,35 @@ export default {
   },
   data() {
     return {
-      list: [
-        {
-          title: "成人票",
-          children: [
-            {
-              title: "成人三馆联票",
-              children: [
-                {
-                  title: "成人三馆联票 - 东尚蜂鸟店"
-                }
-              ]
-            },
-            {
-              title: "成人五馆联票"
-            }
-          ]
-        },
-        {
-          title: "学生票"
-        },
-        {
-          title: "儿童票"
-        },
-        {
-          title: "特惠票"
-        }
-      ]
+      sightName: "",
+      bannerImg: "",
+      gallaryImgs: [],
+      categoryList: []
     };
+  },
+  mounted() {
+    this.getDetailInfo();
+  },
+  methods: {
+    getDetailInfo() {
+      axios
+        .get("/api/detail.json", {
+          params: {
+            id: this.$route.params.id
+          }
+        })
+        .then(this.handleGetDataSucc);
+    },
+    handleGetDataSucc(res) {
+      res = res.data;
+      if (res.ret && res.data) {
+        const data = res.data;
+        this.sightName = data.sightName;
+        this.bannerImg = data.bannerImg;
+        this.gallaryImgs = data.gallaryImgs;
+        this.categoryList = data.categoryList;
+      }
+    }
   }
 };
 </script>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img
-        src="//img1.qunarzz.com/sight/p0/1508/c5/a017645d82ab89f271fdfb1bd6ecc2e0.water.jpg_600x330_370cb642.jpg"
-        alt
-        class="banner-img"
-      >
+      <img :src="bannerImg" alt class="banner-img">
       <div class="banner-info">
-        <div class="banner-title">秦岭野生动物园(AAAA景区)</div>
+        <div class="banner-title">{{ this.name }}</div>
         <div class="banner-number">
-          <span class="iconfont">&#xe65f;</span> 16
+          <span class="iconfont">&#xe65f;</span>
+          {{this.gallaryImgs.length}}
         </div>
       </div>
     </div>
-    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleClose"></common-gallary>
+    <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleClose"></common-gallary>
   </div>
 </template>
 <script>
@@ -22,6 +19,11 @@ export default {
   name: "DetailBanner",
   components: {
     CommonGallary
+  },
+  props: {
+    name: String,
+    bannerImg: String,
+    gallaryImgs: Array
   },
   data() {
     return {
